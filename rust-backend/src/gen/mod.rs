fe0c9b5f93f37ddffe97b36217770aaaa72817a7
Erik Berkun-Drevnig <erik.b22@gmail.com>
2024-10-14 21:21:05 -0400

    Revert "Remove Rust backend"
    This reverts commit d503dc41cfafb8b1b163d7db5b4506336837a1e0.

---
+++ rust-backend/src/gen/mod.rs
@@ -0,0 +1,8 @@
+// @generated
+pub mod weather {
+    // @@protoc_insertion_point(attribute:weather.v1)
+    pub mod v1 {
+        include!("weather.v1.rs");
+        // @@protoc_insertion_point(weather.v1)
+    }
+}
